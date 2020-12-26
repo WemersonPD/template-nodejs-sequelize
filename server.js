15e72005d9ed398570c2fcca01fbaaa0a4855fec
WemersonPD <wpdads10@gmail.com>
2020-12-25 23:44:29 -0300

    Add eslint and prettier

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
 
 require('dotenv').config();
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
-app.use(bodyParser.json({ limit: '100mb' }))
+// Log application
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
 
 // Cors
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*")
-	res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, OPTIONS, DELETE")
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-  next()
-})
+app.use(cors());
 
 // require('./src/app/routes')(app)
 
-let port = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 
 app.get('/', (req, resp) => {
-	resp.send(`App is running at port ${port}`)
-})
+	resp.send(`App is running at port ${PORT}`);
+});
 
-app.listen(port, () => {
-	console.log('Running at port:' + port)
-})
+app.listen(PORT, () => {
+	console.log(`Running at port ${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
